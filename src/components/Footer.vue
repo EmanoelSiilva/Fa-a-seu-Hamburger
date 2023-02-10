12275e43ebb2c03ee98b3a6df4c11bc703f802f6
Emanoel <emanoelsilvaif@gmail.com>
2023-02-09 21:34:42 -0300

    primeiro commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="footer">
+      <p>Faça seu hamburger &copy; 2023</p>
+    </div>
+</template>
+<script>
+  export default {
+    name: "Footer"
+  }
+</script>
+
+<style>
+  #footer {
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #FCBA03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    bottom: 0;
+    width: 100%;
+  }
+</style>
