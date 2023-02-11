3c0fe7d0d1437187471793ee11ce435d9efec044
Emanoel <emanoelsilvaif@gmail.com>
2023-02-10 22:15:08 -0300

    adicionando o CRUD

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -11,22 +11,22 @@
             </div>
         </div>
         <div id="burger-table-rows">
-            <div class="burger-table-row">
-                <div class="order-number">1</div>
-                <div>João</div>
-                <div>Pão de Trigo</div>
-                <div>Maminha</div>
+            <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+                <div class="order-number">{{ burger.id }}</div>
+                <div>{{ burger.nome }}</div>
+                <div>{{ burger.pao }}</div>
+                <div>{{ burger.carne }}</div>
                 <div>
                     <ul>
-                        <li>Salame</li>
-                        <li>Tomate</li>
+                        <li v-for="(opcional, index) in burger.opcionais" :key="index">{{ opcional }}</li>
                     </ul>
                 </div>
                 <div>
                     <select name="status" class="status">
-                        <option value="">Selecione o status do Hamburger</option>
+                        <option value="">Selecione</option>
+                        <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo">{{ s.tipo }}</option>
                     </select>
-                    <button class="delete-btn">Cancelar</button>
+                    <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
                 </div>
             </div>
         </div>
@@ -35,63 +35,95 @@
 
 <script>
 export default {
-    name: "Dashboard"
+    name: "Dashboard",
+    data() {
+        return {
+            burgers: null,
+            burger_id: null,
+            status: []
+        }
+    },
+    methods: {
+        async getPedidos() {
+            const req = await fetch("http://localhost:3000/burgers")
+
+            const data = await req.json()
+            this.burgers = data
+
+            this.getStatus()
+
+        },
+
+        async getStatus() {
+            const req = await fetch("http://localhost:3000/status")
+
+            const data = await req.json()
+            this.status = data
+        },
+
+        async deleteBurger(id) {
+            const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                method: "DELETE"
+            })
+
+            const res = await req.json()
+
+            this.getPedidos()
+        }
+    },
+    mounted() {
+        this.getPedidos()
+    }
 }
 </script>
 
 <style scoped>
-    #burger-table {
-        max-width: 1200px;
-        margin: 0 auto;
-    }
-
-    #burger-table-heading,
-    #burger-table-rows,
-    #burger-table-row {
-        display: flex;
-        flex-wrap: wrap;
-    }
-
-    #burger-table-heading {
-        font-weight: bold;
-        padding: 12px;
-        border-bottom: 3px solid #333;
-    }
-
-    #burger-table-heading div,
-    .burger-table-row div {
-        width: 19%;
-    }
-
-    .burger-table-row {
-        width: 100%;
-        padding: 12px;
-        border-bottom: 1px solid #ccc;
-    }
-
-    #burger-table-heading .order-id,
-    .burger-table-row .order-number {
-        width: 5%;
-    }
-
-    select {
-        padding: 12px 6px;
-        margin-right: 12px;
-    }
-
-    .delete-btn {
-        background-color: #222;
-        color: #FCBA03;
-        font-weight: bold;
-        border: 2px, solid #222;
-        font-size: 16px;
-        margin: 0 auto;
-        cursor: pointer;
-        transition: .5s;
-    }
-
-    .delete-btn:hover {
-        background-color: transparent;
-        color: #222;
-    }
+      #burger-table {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+  #burger-table-heading,
+  #burger-table-rows,
+  .burger-table-row {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  #burger-table-heading {
+    font-weight: bold;
+    padding: 12px;
+    border-bottom: 3px solid #333;
+  }
+  .burger-table-row {
+    width: 100%;
+    padding: 12px;
+    border-bottom: 1px solid #CCC;
+  }
+  #burger-table-heading div,
+  .burger-table-row div {
+    width: 19%;
+  }
+  #burger-table-heading .order-id,
+  .burger-table-row .order-number {
+    width: 5%;
+  }
+  select {
+    padding: 12px 6px;
+    margin-right: 12px;
+  }
+  .delete-btn {
+    background-color: #222;
+    color:#fcba03;
+    font-weight: bold;
+    border: 2px solid #222;
+    padding: 10px;
+    font-size: 16px;
+    margin: 0 auto;
+    cursor: pointer;
+    transition: .5s;
+  }
+  
+  .delete-btn:hover {
+    background-color: transparent;
+    color: #222;
+  }
 </style>
